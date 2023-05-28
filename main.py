ffe3ff5a99a4c8805ff7b8e86f07639b55bb6122
sta-alex <80824722+sta-alex@users.noreply.github.com>
2023-05-28 21:31:23 +0200

    html finished

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import folium
 import geopandas
 import pandas
 from flask import Flask, render_template, request
+
+
 def get_GeoJSON():
     url = "https://bikeshare.metro.net/stations/json"
     headers = {
@@ -30,6 +32,12 @@ def get_GeoJSON():
                 json.dump(data, file)
 
             print("Data saved successfully.")
+            json_data = "data/geo_data.json"
+            jsonfile = open(json_data)
+            # geopanda dataframe
+            dfgeo = geopandas.read_file(jsonfile)
+            jsonfile.close()
+            dfgeo.to_csv("data/geo_station_live.csv")
         else:
             if os.path.exists(data_file):
                 # Load the data from the stored file
@@ -49,33 +57,41 @@ def get_GeoJSON():
             print("An error occurred. Using stored data.")
         else:
             print("An error occurred. No stored data available.")
+
+
 # retrieves the GeoJSon from the Metro Bike Station Website
 get_GeoJSON()
 
-json_data = "data/geo_data.json"
-jsonfile = open(json_data)
-# geopanda dataframe
-dfgeo = geopandas.read_file(jsonfile)
-# convert to pada dataframe
-df = dfgeo.drop(columns='geometry')
-jsonfile.close()
-dfgeo.to_csv("data/geo_station_live.csv")
-df.to_csv("data/station_live.csv")
 
-def create_local_html_map():
+def create_csv():  # creates the csv files from json and returns geodataframe
+    json_data = "data/geo_data.json"
+    jsonfile = open(json_data)
+    # geopanda dataframe
+    dfgeo = geopandas.read_file(jsonfile)
+    jsonfile.close()
+    dfgeo.to_csv("data/geo_station_live.csv")
+    return dfgeo
+
+
+# creates the csv files from json and returns geodataframe
+df = create_csv()
+
+
+def create_local_html_map(poslat, poslong):
     # Create a map centered at a specific location
-    m = folium.Map(location=[34.04919, -118.24799], zoom_start=10)
+    m = folium.Map(location=[poslat, poslong], zoom_start=10)
 
     # Add a marker to the map
-    folium.Marker(location=[34.04919, -118.24799], popup="Los Angeles").add_to(m)
+    folium.Marker(location=[poslat, poslong], popup=f'Postition:\nlat: {poslat}\nlong: {poslong}').add_to(m)
+    # Add Station Marker
 
     # Save the map as an HTML file
     map_file = 'templates/map.html'
     m.save(map_file)
 
-## Call the function to create and load the local HTML map
-create_local_html_map()
 
+# Call the function to create and load the local HTML map
+create_local_html_map(34.04919, -118.24799)
 
 
 def run_map_viewer():
@@ -84,13 +100,17 @@ def run_map_viewer():
     @app.route('/', methods=['GET', 'POST'])
     def index():
         if request.method == 'POST':
-            latitude = request.form['latitude']
-            longitude = request.form['longitude']
+
+            latitude = float(request.form['latitude'])
+            longitude = float(request.form['longitude'])
+            create_local_html_map(latitude or 34.04919, longitude or -118.24799)
             return render_template('index.html', latitude=latitude, longitude=longitude)
+
         return render_template('index.html')
 
     if __name__ == '__main__':
         app.run(debug=True)
+
 
 if __name__ == '__main__':
     run_map_viewer()
