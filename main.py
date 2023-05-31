883f05373c5bae352bf4ce461973696aace8f4f5
sta-alex <80824722+sta-alex@users.noreply.github.com>
2023-05-31 02:51:50 +0200

    make it pretty

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ import geopandas
 import pandas
 from flask import Flask, render_template, request
 from shapely.geometry import Point
+import osmnx as ox
+import networkx as nx
 
 # default starting point Los Angeles
 default_latitude, default_longitude = 34.04919, -118.24799
 k_number_default = 5
 crs_code_format = "EPSG:3857"
-drop_if_number = 8
 
 
 def get_GeoJSON():
@@ -37,24 +38,28 @@ def get_GeoJSON():
             # Save the data to a file
             with open(data_file, "w") as file:
                 json.dump(data, file)
-
             print("Data saved successfully.")
+
             json_data = "data/geo_data.json"
             jsonfile = open(json_data)
-            # geopanda dataframe
+
+            # create a save csv file
             dfgeo = geopandas.read_file(jsonfile)
             jsonfile.close()
             dfgeo.to_csv("data/geo_station_live.csv")
-        else:
+
+        else:  # e.g. response.status_code 404
+
             if os.path.exists(data_file):
                 # Load the data from the stored file
                 with open(data_file, "r") as file:
                     data = json.load(file)
-
                 print("Failed to retrieve data. Using stored data.")
+
             else:
                 print(f"Request failed with status code: {response.status_code}")
                 print("No stored data available.")
+
     except requests.exceptions.RequestException as e:
         if os.path.exists(data_file):
             # Load the data from the stored file
@@ -84,14 +89,15 @@ def create_dataframe():  # creates the Geodataframe and creates a csv
 # df = create_dataframe()
 
 
-def create_local_html_map(dataframe, poslat, poslong, k_number):
+def create_local_html_map(dataframe, poslat, poslong, k_nearest):
+
     # Create a map centered at a specific location
     m = folium.Map(location=[poslat, poslong],
                    zoom_start=15,
                    control_scale=True,
                    crs='EPSG3857'
                    )
-    k_nearest = int(k_number)
+
     # Add My Position marker to the map
     my_position = folium.Marker(location=[poslat, poslong],
                                 popup=f'Postition:\nlat: {poslat}\nlong: {poslong}',
@@ -99,97 +105,159 @@ def create_local_html_map(dataframe, poslat, poslong, k_number):
                                 icon=folium.Icon(color='black', icon="user")
                                 ).add_to(m)
 
-    # Create a point of the current position
-    my_position_point = Point(poslong, poslat)
-    my_position_point = geopandas.GeoSeries([my_position_point], crs="EPSG:4326").to_crs("EPSG:3857").iloc[0]
-    print(my_position_point)
-    # Create a geodataframe only with necessary columns
-    gdf = dataframe.loc[:, ('kioskId', 'name', 'addressStreet', 'addressCity', 'addressState', 'addressZipCode',
-                            'latitude', 'longitude', 'geometry')]
-    # Reproject the 'geometry' column to the desired CRS format
-    gdf['geometry'] = gdf['geometry'].to_crs(crs_code_format)
-    # Calculate the distances from the current position to all stations
-    gdf['distance'] = gdf['geometry'].distance(my_position_point)
-    gdf = gdf.sort_values(by='distance')
+    # returns a dataframe containing the k_nearest stations
+    df_nearest = get_nearest_dataframe(dataframe, poslong, poslat, k_nearest)
 
-    # Find the k-nearest Station
-    deleted_kiosk_id = []
-
-    for num in range(k_nearest):
-        spatial_index = gdf.sindex
-        nearest_geometry = spatial_index.nearest(my_position_point, return_all=False)
-
-        nearest_indicies = nearest_geometry.flatten()[:1]
-        nearest_stations = gdf.iloc[nearest_indicies]
-        deleted_kiosk_id.extend(nearest_stations['kioskId'].tolist())
-
-        gdf = gdf.drop(gdf.index[nearest_indicies.tolist()])
-
-    df_nearest = dataframe[dataframe['kioskId'].isin(deleted_kiosk_id)]
     print(df_nearest)
 
-    # Add Station Markers to the map from the geodataframe
+    # Add Station Markers to the map from the Geo dataframe
     for idx, row in dataframe.iterrows():
         long, lat = row['geometry'].x, row['geometry'].y
 
-        if 'Active' in row['kioskPublicStatus'] and int(row['bikesAvailable']) > 5 and int(row['docksAvailable']) > 5:
-            if row['kioskId'] in df_nearest['kioskId'].values:
-                color = 'darkgreen'
-            else:
-                color = 'lightgreen'
-            icon = 'ok-sign'
-        elif 'Active' in row['kioskPublicStatus'] and int(row['bikesAvailable']) >= 2 and int(
-                row['docksAvailable']) >= 2:
-            if row['kioskId'] in df_nearest['kioskId'].values:
-                color = 'darkblue'
-            else:
-                color = 'lightblue'
-            icon = 'info-sign'
-        elif 'Unavailable' in row['kioskPublicStatus'] or 'Active' in row['kioskPublicStatus']:
-            if row['kioskId'] in df_nearest['kioskId'].values:
-                color = 'darkred'
-            else:
-                color = 'lightred'
-            icon = 'remove-sign'
-        else:
-            color = 'gray'
-            icon = 'search'
-
-        station_name = row['name']
-
-        popup_html = """
-        <b>Name:</b> {}<br>
-        <b>ID:</b> {}<br>
-        <b>Street:</b> {}<br>
-        <b>Available Bikes:</b> {}<br>
-        <b>Available Docks:</b> {}<br>
-        <b>Status:</b> {}<br>
-        <b>Opening hours:</b> {} am - {} pm<br>
-        """.format(row['name'], row['kioskId'], row['addressStreet'], row['bikesAvailable'], row['docksAvailable'],
-                   row['kioskPublicStatus'], row['openTime'], row['closeTime'])
-        popup = folium.Popup(html=popup_html, max_width=250)
-        station_marker = folium.Marker(location=[lat, long],
-                                       tooltip=f'{station_name}',
-                                       popup=popup,
-                                       icon=folium.Icon(color=f'{color}',
-                                                        icon=f'{icon}')
-                                       ).add_to(m)
+        # retrieves right color and icon
+        color, icon = icon_color(row, df_nearest)
+        # creates marker for map
+        station_marker = create_markers(row, lat, long, color, icon)
+        # add marker to map
+        station_marker.add_to(m)
 
     # Add Clickable map that Copies lat and long to clipboard
     m.add_child(folium.ClickForLatLng())
+
     # Save the map as an HTML file
+    # map_file = 'templates/map.html'
+    # m.save(map_file)
+    save_map(m)
+    return df_nearest, m
+
+
+def get_nearest_dataframe(dataframe, poslong, poslat, k_nearest):
+    my_position_point = Point(poslong, poslat)
+    # Convert the point to the needed crs system
+    my_position_point = geopandas.GeoSeries([my_position_point], crs="EPSG:4326").to_crs("EPSG:3857").iloc[0]
+
+    # Create a geodataframe only with necessary columns
+    gdf = dataframe.loc[:, ('kioskId', 'name', 'addressStreet', 'addressCity', 'addressState', 'addressZipCode',
+                            'latitude', 'longitude', 'geometry')]
+
+    # Reproject the 'geometry' column to the desired CRS format
+    gdf['geometry'] = gdf['geometry'].to_crs(crs_code_format)
+
+    # Calculate the distances from the current position to all stations
+    gdf['distance'] = gdf['geometry'].distance(my_position_point)
+    # sort the Dataframe by distances and get the k_nearest neighbours
+    gdf = gdf.sort_values(by='distance')
+    df_nearest = dataframe[dataframe['kioskId'].isin(gdf['kioskId'].head(k_nearest))]
+
+    return df_nearest
+
+
+def icon_color(row, df_nearest):
+    if 'Active' in row['kioskPublicStatus'] and int(row['bikesAvailable']) > 5 and int(row['docksAvailable']) > 5:
+        if row['kioskId'] in df_nearest['kioskId'].values:
+            color = 'darkgreen'
+        else:
+            color = 'lightgreen'
+        icon = 'ok-sign'
+    elif 'Active' in row['kioskPublicStatus'] and int(row['bikesAvailable']) >= 2 and int(
+            row['docksAvailable']) >= 2:
+        if row['kioskId'] in df_nearest['kioskId'].values:
+            color = 'darkblue'
+        else:
+            color = 'lightblue'
+        icon = 'info-sign'
+    elif 'Unavailable' in row['kioskPublicStatus'] or 'Active' in row['kioskPublicStatus']:
+        if row['kioskId'] in df_nearest['kioskId'].values:
+            color = 'darkred'
+        else:
+            color = 'lightred'
+        icon = 'remove-sign'
+    else:
+        color = 'gray'
+        icon = 'search'
+    return color, icon
+
+
+def create_markers(row, lat, long, color, icon):
+    station_name = row['name']
+
+    popup_html = """
+            <b>Name:</b> {}<br>
+            <b>ID:</b> {}<br>
+            <b>Street:</b> {}<br>
+            <b>Available Bikes:</b> {}<br>
+            <b>Available Docks:</b> {}<br>
+            <b>Status:</b> {}<br>
+            <b>Opening hours:</b> {} am - {} pm<br>
+            """.format(row['name'], row['kioskId'], row['addressStreet'], row['bikesAvailable'], row['docksAvailable'],
+                       row['kioskPublicStatus'], row['openTime'], row['closeTime'])
+
+    popup = folium.Popup(html=popup_html, max_width=250)
+
+    station_marker = folium.Marker(location=[lat, long],
+                                   tooltip=f'{station_name}',
+                                   popup=popup,
+                                   icon=folium.Icon(color=f'{color}',
+                                                    icon=f'{icon}')
+                                   )
+    return station_marker
+
+
+def save_map(m):
     map_file = 'templates/map.html'
     m.save(map_file)
-    return df_nearest
+
+
+def find_route(start_point, end_point, travel_type):
+    place_name = 'Los Angeles, California, USA'
+
+    graph = ox.graph_from_place(place_name, network_type=travel_type)
+
+    orig_node = ox.distance.nearest_nodes(graph, start_point.x, start_point.y)
+    dest_node = ox.distance.nearest_nodes(graph, end_point.x, end_point.y)
+
+    route = nx.shortest_path(graph, orig_node, dest_node, weight="length")
+
+    route_coordinates = [(graph.nodes[node]["y"], graph.nodes[node]["x"]) for node in route]
+
+    return route_coordinates
+
+
+def create_point(lat, long):
+    point = Point(long, lat)
+    point = geopandas.GeoSeries([point], crs='EPSG:3857')
+    point = point.to_crs('EPSG:4326')
+    # point = geopandas.GeoSeries([point], crs="EPSG:4326").to_crs("EPSG:3857").iloc[0]
+    print(point)
+    return point.iloc[0]
+
+
+def select_bikes(df, drop_numbers):
+    df = df.loc[:, ('kioskId', 'name', 'addressStreet', 'addressCity', 'addressState', 'addressZipCode',
+                    'bikesAvailable', 'classicBikesAvailable', 'smartBikesAvailable',
+                    'electricBikesAvailable',
+                    'docksAvailable', 'kioskPublicStatus', 'openTime', 'closeTime',
+                    'latitude', 'longitude', 'geometry',)] \
+        .drop(df[df['bikesAvailable'] <= drop_numbers].index)
+    return df
+
+
+def select_docks(df, drop_numbers):
+    df = df.loc[:, ('kioskId', 'name', 'addressStreet', 'addressCity', 'addressState', 'addressZipCode',
+                    'bikesAvailable', 'docksAvailable', 'kioskPublicStatus', 'openTime', 'closeTime',
+                    'latitude', 'longitude', 'geometry',)] \
+        .drop(df[df['docksAvailable'] <= drop_numbers].index)
+    return df
 
 
 def run_map_viewer():
     app = Flask(__name__)
 
-    @app.route('/', methods=['GET', 'POST', 'ROUTE'])
+    @app.route('/', methods=['GET', 'POST'])
     def index():
         df = create_dataframe()
         if request.method == 'POST':
+            # Get inputs from Website
             latitude = float(request.form['latitude'])
             longitude = float(request.form['longitude'])
             rankings = request.form.get('rankings')
@@ -198,30 +266,37 @@ def run_map_viewer():
             dest_longitude = request.form.get('destLong')
             search_bikes = request.form.get('searchBike') == 'on'
             search_docks = request.form.get('searchDocks') == 'on'
+            drop_if_number = request.form.get('available_pieces')
+            drop_if_number = int(drop_if_number) if drop_if_number else 1
 
             if dest_longitude and dest_latitude:
                 print("________________________ROUTING STARTED________________________")
                 rankings = 1
-            if search_bikes:
-                df = df.loc[:, ('kioskId', 'name', 'addressStreet', 'addressCity', 'addressState', 'addressZipCode',
-                                'bikesAvailable', 'classicBikesAvailable', 'smartBikesAvailable',
-                                'electricBikesAvailable',
-                                'docksAvailable', 'kioskPublicStatus', 'openTime', 'closeTime',
-                                'latitude', 'longitude', 'geometry',)] \
-                    .drop(df[df['bikesAvailable'] <= drop_if_number].index)
-            elif search_docks:
-                df = df.loc[:, ('kioskId', 'name', 'addressStreet', 'addressCity', 'addressState', 'addressZipCode',
-                                'bikesAvailable', 'docksAvailable', 'kioskPublicStatus', 'openTime', 'closeTime',
-                                'latitude', 'longitude', 'geometry',)] \
-                    .drop(df[df['docksAvailable'] <= drop_if_number].index)
+                travel_mode = "bike"
+                source_point = create_point(latitude, longitude)
+                dest_point = create_point(dest_latitude, dest_longitude)
+                route_coordinates = find_route(source_point, dest_point, travel_mode)
+                print(route_coordinates)
+                gdf, m = create_local_html_map(df, latitude or default_latitude, longitude or default_longitude,
+                                               rankings)
+                folium.PolyLine(locations=route_coordinates, color='blue', weight=3).add_to(m)
+                save_map(m)
+                print("________________________ROUTING END________________________")
 
-            gdf = create_local_html_map(df, latitude or default_latitude, longitude or default_longitude, rankings)
+            # prepare Task1
+            if search_bikes:
+                df = select_bikes(df, drop_if_number)
+            # prepare Task2
+            elif search_docks:
+                df = select_docks(df, drop_if_number)
+
+            gdf, m = create_local_html_map(df, latitude or default_latitude, longitude or default_longitude, rankings)
 
             return render_template('index.html', latitude=latitude, longitude=longitude,
                                    search_bikes=search_bikes, search_docks=search_docks,
                                    df_html=gdf.to_html(index=False))
 
-        gdf = create_local_html_map(df, default_latitude, default_longitude, k_number_default)
+        gdf, m = create_local_html_map(df, default_latitude, default_longitude, k_number_default)
         return render_template('index.html', latitude=default_latitude, longitude=default_longitude,
                                df_html=gdf.to_html(index=False))
 
