0dfa75f93f5d098dec1cbd3f08507a4ac5d5fb09
sta-alex <80824722+sta-alex@users.noreply.github.com>
2023-06-01 02:01:14 +0200

    working, no route visible using open api

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-import requests
-import os
 import json
+import os
 import folium
 import geopandas
 import pandas
+import requests
 from flask import Flask, render_template, request
 from shapely.geometry import Point
-import osmnx as ox
-import networkx as nx
 
 # default starting point Los Angeles
 default_latitude, default_longitude = 34.04919, -118.24799
 k_number_default = 5
-crs_code_format = "EPSG:3857"
+crs_routing_format = "EPSG:4326"
+crs_map_format = "EPSG:3857"
+api_ORS_key = "5b3ce3597851110001cf62483a64689c0c234ddab368b092813c9dce"
+by_bike = "cycling-regular"
+by_foot = "foot-walking"
 
 
 def get_GeoJSON():
@@ -89,7 +91,8 @@ def create_dataframe():  # creates the Geodataframe and creates a csv
 # df = create_dataframe()
 
 
-def create_local_html_map(dataframe, poslat, poslong, k_nearest, destpoint=Point(0,0), route_coordinates=0):
+def create_local_html_map(dataframe, poslat, poslong, k_nearest, destlat=0.0, destlong=0.0,
+                          route_coordinates=geopandas.GeoDataFrame()):
 
     # Create a map centered at a specific location
     m = folium.Map(location=[poslat, poslong],
@@ -108,17 +111,18 @@ def create_local_html_map(dataframe, poslat, poslong, k_nearest, destpoint=Point
     # returns a dataframe containing the k_nearest stations
     df_nearest = get_nearest_dataframe(dataframe, poslong, poslat, k_nearest)
 
-    if not (destpoint and route_coordinates) == 0:
-        folium.Marker(location=[destpoint.x, destpoint.y],
-                      popup=f'Postition:\nlat: {destpoint.x}\nlong: {destpoint.y}',
+    if not (destlat and destlong) == 0:
+        folium.Marker(location=[destlat, destlong],
+                      popup=f'Postition:\nlat: {destlat}\nlong: {destlong}',
                       tooltip='My Position Destination',
                       icon=folium.Icon(color='black', icon="user")
                       ).add_to(m)
         folium.PolyLine(locations=route_coordinates, color='blue', weight=3).add_to(m)
-        df_nearest_route = get_nearest_dataframe(dataframe, destpoint.x, destpoint.y, k_nearest)
-        df_nearest = geopandas.GeoDataFrame.merge(df_nearest, df_nearest_route)
 
-    print(df_nearest)
+        df_nearest_route = get_nearest_dataframe(dataframe, destlong, destlat, k_nearest)
+        df_nearest = pandas.concat([df_nearest, df_nearest_route])
+        print("-------------------Combined Dataframe-------------------")
+        print(df_nearest)
 
     # Add Station Markers to the map from the Geo dataframe
     for idx, row in dataframe.iterrows():
@@ -131,8 +135,6 @@ def create_local_html_map(dataframe, poslat, poslong, k_nearest, destpoint=Point
         # add marker to map
         station_marker.add_to(m)
 
-
-
     # Add Clickable map that Copies lat and long to clipboard
     m.add_child(folium.ClickForLatLng())
 
@@ -143,16 +145,16 @@ def create_local_html_map(dataframe, poslat, poslong, k_nearest, destpoint=Point
 
 
 def get_nearest_dataframe(dataframe, poslong, poslat, k_nearest):
-    my_position_point = Point(poslong, poslat)
+    # my_position_point = Point(poslong, poslat)
     # Convert the point to the needed crs system
-    my_position_point = geopandas.GeoSeries([my_position_point], crs="EPSG:4326").to_crs("EPSG:3857").iloc[0]
-
+    # my_position_point = geopandas.GeoSeries([my_position_point], crs="EPSG:4326").to_crs("EPSG:3857").iloc[0]
+    my_position_point = create_point(poslat, poslong, crs_routing_format, crs_map_format)
     # Create a geodataframe only with necessary columns
     gdf = dataframe.loc[:, ('kioskId', 'name', 'addressStreet', 'addressCity', 'addressState', 'addressZipCode',
                             'latitude', 'longitude', 'geometry')]
 
     # Reproject the 'geometry' column to the desired CRS format
-    gdf['geometry'] = gdf['geometry'].to_crs(crs_code_format)
+    gdf['geometry'] = gdf['geometry'].to_crs(crs_map_format)
 
     # Calculate the distances from the current position to all stations
     gdf['distance'] = gdf['geometry'].distance(my_position_point)
@@ -219,27 +221,67 @@ def save_map(m):
     m.save(map_file)
 
 
-def find_route(start_point, end_point, travel_type):
-    place_name = 'Los Angeles, California, USA'
+# def find_route(source_lat, source_long, dest_lat, dest_long, travel_type):
+#    # create points in "EPSG:4326"
+#    start_point = create_point(source_lat, source_long, crs_map_format, crs_routing_format)
+#    end_point = create_point(dest_lat, dest_long, crs_map_format, crs_routing_format)
+#    north_lat = 34.224313
+#    south_lat = 33.906345
+#    east_long = -118.180111
+#    west_long = -118.529665
+#    graph = osmnx.graph_from_bbox(north_lat, south_lat, east_long, west_long, network_type=travel_type, simplify=True)
+#    print("Graph_success")
+#    #new
+#    start_point = (source_lat, source_long)
+#    end_point = (dest_lat, dest_long)
+#    #find nearest nodes
+#    n_source = osmnx.distance.nearest_nodes(graph, source_long, source_lat, return_dist=True)
+#    n_dest = osmnx.distance.nearest_nodes(graph, dest_long, dest_lat, return_dist=True)
+#    osmnx_route = networkx.shortest_path(graph, n_source, n_dest, 'length')
+#
+#    edge_lengths = osmnx.utils_graph.get_route_edge_attributes(
+#                    graph, osmnx_route, 'length')
+#    route_len_m = sum(edge_lengths)
+#    print(route_len_m)
+#    return route_len_m
 
-    graph = ox.graph_from_place(place_name, network_type=travel_type)
+def find_route(source_lat, source_long, dest_lat, dest_long, travel_type):
+    route_folder = "routes"
+    data_file = os.path.join(route_folder, "geo_data_route.json")
+    #
+    if not os.path.exists(route_folder):
+        os.makedirs(route_folder)
 
-    orig_node = ox.distance.nearest_nodes(graph, start_point.x, start_point.y)
-    dest_node = ox.distance.nearest_nodes(graph, end_point.x, end_point.y)
+    headers = {
+        'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
+    }
+    call = requests.get(
+        f'https://api.openrouteservice.org/v2/directions/{travel_type}?api_key={api_ORS_key}&start={source_long},{source_lat}&end={dest_long},{dest_lat}',
+        headers=headers)
 
-    route = nx.shortest_path(graph, orig_node, dest_node, weight="length")
+    data = json.loads(call.text)
+    # Save the data to a file
+    with open(data_file, "w") as file:
+        json.dump(data, file)
+    #
+    print(" Route Data saved successfully.")
+    #
+    #
+    route_folder = "routes"
+    data_file = os.path.join(route_folder, "geo_data_route.json")
+    with open(data_file, 'r') as f:
+        geojson_data = json.load(f)
 
-    route_coordinates = [(graph.nodes[node]["y"], graph.nodes[node]["x"]) for node in route]
+    coordinates = geojson_data['features'][0]['geometry']['coordinates']
 
-    return route_coordinates
+    return coordinates
 
 
-def create_point(lat, long):
+def create_point(lat, long, crs_in, crs_out):
     point = Point(long, lat)
-    point = geopandas.GeoSeries([point], crs='EPSG:3857')
-    point = point.to_crs('EPSG:4326')
-    # point = geopandas.GeoSeries([point], crs="EPSG:4326").to_crs("EPSG:3857").iloc[0]
-    print(point)
+    point = geopandas.GeoSeries([point], crs=crs_in)
+    point = point.to_crs(crs_out)
+    print(f'Point from {crs_in} to {crs_out}: from {lat} , {long} to {point.iloc[0]}')
     return point.iloc[0]
 
 
@@ -261,41 +303,53 @@ def select_docks(df, drop_numbers):
     return df
 
 
+def request_lat_long(in_put):
+    out_put = request.form.get(in_put)
+    if out_put:
+        out_put = float(out_put)
+    else:
+        out_put = 0.0
+    return out_put
+
+
 def run_map_viewer():
     app = Flask(__name__)
 
     @app.route('/', methods=['GET', 'POST'])
     def index():
         df = create_dataframe()
+        gdf = df
         if request.method == 'POST':
-            # Get inputs from Website
-            latitude = float(request.form['latitude'])
-            longitude = float(request.form['longitude'])
+
+            # Get float inputs from Website
+            latitude = request_lat_long('latitude')
+            longitude = request_lat_long('longitude')
+            dest_latitude = request_lat_long('destLat')
+            dest_longitude = request_lat_long('destLong')
+
+            # Get int inputs from Website
             rankings = request.form.get('rankings')
             rankings = int(rankings) if rankings else 3
-            dest_latitude = request.form.get('destLat')
-            dest_longitude = request.form.get('destLong')
-            search_bikes = request.form.get('searchBike') == 'on'
-            search_docks = request.form.get('searchDocks') == 'on'
             drop_if_number = request.form.get('available_pieces')
             drop_if_number = int(drop_if_number) if drop_if_number else 1
+            # Get Checkbox from Website
+            search_bikes = request.form.get('searchBike') == 'on'
+            search_docks = request.form.get('searchDocks') == 'on'
 
             # prepare Task1
             if search_bikes:
                 df = select_bikes(df, drop_if_number)
             # prepare Task2
-            elif search_docks:
+            if search_docks:
                 df = select_docks(df, drop_if_number)
-            elif dest_longitude and dest_latitude:
+            if dest_longitude and dest_latitude:
                 print("________________________ROUTING STARTED________________________")
                 rankings = 1
-                travel_mode = "bike"
-                source_point = create_point(latitude, longitude)
-                dest_point = create_point(dest_latitude, dest_longitude)
-                route_coordinates = find_route(source_point, dest_point, travel_mode)
+                travel_mode = by_bike
+                route_coordinates = find_route(latitude, longitude, dest_latitude, dest_longitude, travel_mode)
                 print("________________________ROUTING END________________________")
                 gdf, m = create_local_html_map(df, latitude or default_latitude, longitude or default_longitude,
-                                               rankings, dest_point, route_coordinates)
+                                               rankings, dest_latitude, dest_longitude, route_coordinates)
             else:
                 gdf, m = create_local_html_map(df, latitude or default_latitude, longitude or default_longitude,
                                                rankings)
@@ -305,6 +359,7 @@ def run_map_viewer():
                                    df_html=gdf.to_html(index=False))
 
         gdf, m = create_local_html_map(df, default_latitude, default_longitude, k_number_default)
+
         return render_template('index.html', latitude=default_latitude, longitude=default_longitude,
                                df_html=gdf.to_html(index=False))
 
